abbeecb83eedbc431563ff4d7484eab91a0cc2d7
Joey <Fangjin98@outlook.com>
2022-10-15 10:22:55 +0800

    init

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-scp -r -P 6002  ~/fj/distributed-layer-INA sdn@172.16.160.2:~/fj/
-scp -r -P 6008  ~/fj/distributed-layer-INA sdn@172.16.160.8:~/fj/
-scp -r ~/fj/distributed-layer-INA sdn@172.16.150.31:~/fj/
--- test.sh
+++
@@ -1,3 +0,0 @@
-WORKER_NUM=$1
-
-sudo python3 src/launch.py --master 1 --ip 172.16.210.2 --worker_num $WORKER_NUM --config_file config/workers.json --dataset CIFAR100 --model resnet50
